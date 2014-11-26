040eac86b463f1757a1c8c3dc1d1c91b22e8fb50
George C. Privon <george@privon.com>
2014-11-25 22:04:03 -0300

    fix a few bugs. add a loop option

--- play_spectra.py
+++ play_spectra.py
@@ -10,17 +10,25 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Play a 1D spectrum through a \
                                               midi output.')
-parser.add_argument('-m', '--midi', type=int, nargs=1, default=0,
+parser.add_argument('-m', '--midi', type=int, default=0,
                     help='MIDI port to use.')
-parser.add_argument('spec', type=str, nargs=1, default=False,
+parser.add_argument('-l', '--loop', default=False, action='store_true',
+                    help='Infinitely loop the spectrum?')
+parser.add_argument('spec', type=str, default=False,
                     help='Spectrum to play.')
 args=parser.parse_args()
 
+print args.spec
+
 sp = pyspeckit.Spectrum(args.spec)
 pygame.midi.init()
-midi_out = pygame.midi.Output(args.m, 0)
-for a in sp.flux:
-    if a > 0:
-        midi_out.note_on(int(((a - min(sp.flux))/max(sp.flux)*24)+50), 1)
-        time.sleep(0.1)
-        midi_out.note_off(int(((a - min(sp.flux))/max(sp.flux)*24)+50))
+midi_out = pygame.midi.Output(args.midi, 0)
+while True:
+    for a in sp.flux:
+        if a > 0:
+            midi_out.note_on(int(((a - min(sp.flux))/max(sp.flux)*30)+0), 1)
+            time.sleep(0.15)
+            midi_out.note_off(int(((a - min(sp.flux))/max(sp.flux)*30)+0))
+    if not(args.loop):
+        break
+    time.sleep(0.5)
